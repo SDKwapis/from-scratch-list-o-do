c03d1ce47c57fe79a6548136942317c70e4765cd
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2024-07-16 12:42:12 -0400

    setup connection.js, set up models, made db tables

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const sequelize = require('sequelize');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+sequelize.sync().then(() => {
 app.listen(PORT, () => {
     console.log('We are Listening, We Are Legion');
+    })
 });
